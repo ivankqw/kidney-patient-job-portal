8ffef93c796df16ca1586d6739234c610f2e28d0
Khoo Cho Yaw, Valentin <valentin_khoo@outlook.com>
2023-02-08 23:43:17 +0800

    Update Profile.vue
    add mobility status and skills

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,75 +1,109 @@
 <template>
   <h3> Welcome back, {{ $store.state.name }} </h3>
   <h3> Your email is {{ $store.state.email }} </h3>
-  <h3> Your data: {{  }}</h3>
+  <h3> Your mobility: {{ this.profile.mobilitystatus }}</h3>
+  <h3> Your skills and past experience: {{  this.profile.skills }} </h3>
   <form @submit.prevent="submitForm">
     <div>
       <label>Mobility status:</label>
-      <select v-model="mobilitystatus" class="form-control form-control-lg" placeholder="Select User Type">
+      <select v-model="profile.mobilitystatus" class="form-control form-control-lg" placeholder="Select User Type" >
         <option value="Sedentary">Sedentary</option>
         <option value="Low Activity">Low Activity</option>
         <option value="Medium Activity">Medium Activity</option>
         <option value="High Activity">High Activity</option>
       </select>
+
+      <label for="skills">Skills and past experience</label><br>
+      <textarea name="skills" id="" cols="100" rows="10" v-model="profile.skills"></textarea>
     </div>
-    
+
     <button type="submit">Save</button>
   </form>
 </template>
 
 <script>
 import { db } from '@/firebase';
-import { setDoc, doc, getDoc } from "firebase/firestore";
+import { setDoc, doc, getDoc, onSnapshot } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
+import { onBeforeUpdate } from 'vue';
 
 export default {
   name: "Home",
   data() {
     return {
-      mobilitystatus: '',
+      profile: {
+        mobilitystatus: '',
+        skills: '',
+      },
+      userdata: 'null',
+      currentDoc: null
     };
   },
+  
+  async mounted() {
+
+    const auth = getAuth();
+    const userEmail = auth.currentUser.email;
+    onSnapshot(doc(db, "Users", userEmail),
+    { includeMetadataChanges: true },
+      (doc) => {
+        this.profile.mobilitystatus = doc.data().mobilitystatus;
+        this.profile.skills = doc.data().skills
+        
+      });
+
+      this.currentDoc = await getDoc(doc(db, "Users", userEmail));
+
+  },
+
   methods: {
     async submitForm() {
       const auth = getAuth();
       const userEmail = auth.currentUser.email;
-
-      try {
-        const currentDoc = await getDoc(doc(db, "Users", userEmail));
-        let updatedData = {};
-        if (currentDoc.exists) {
-          updatedData = {
-            ...currentDoc.data(),
-            mobilitystatus: this.mobilitystatus,
-          };
-        } else {
-          updatedData = {
-            email: userEmail,
-            mobilitystatus: this.mobilitystatus,
-          };
-        }
+      // mobility
+      if (this.profile.mobilitystatus != '') {
         try {
-          await setDoc(doc(db, "Users", userEmail), updatedData);
-        } catch (error) {
-          console.log(error);
-        }
+        this.currentDoc = await getDoc(doc(db, "Users", userEmail));
+        let updatedData = {
+          ...this.currentDoc.data(),
+          mobilitystatus: this.profile.mobilitystatus,
+        };
+        await setDoc(doc(db, "Users", userEmail), updatedData);
         this.mobilitystatus = '';
         alert('saved successfully!');
       } catch (error) {
         console.error(error);
         alert('An error occurred while saving ');
       }
+      }
+
+      if (this.profile.skills != '') {
+        try {
+        this.currentDoc = await getDoc(doc(db, "Users", userEmail));
+        let updatedData = {
+          ...this.currentDoc.data(),
+          skills: this.profile.skills,
+        };
+        await setDoc(doc(db, "Users", userEmail), updatedData);
+        this.profile.skills = '';
+        alert('saved successfully!');
+      } catch (error) {
+        console.error(error);
+        alert('An error occurred while saving ');
+      }
+      }
+      
     },
 
     mounted() {
       const auth = getAuth();
       const userEmail = auth.currentUser.email;
-      
+
     }
   },
 
-  
-  
+
+
 };
 </script>
 <style>
